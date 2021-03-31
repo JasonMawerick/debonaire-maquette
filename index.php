945e22b44df1d59bdad7d67a826bbc70c3689c0f
JasonMawerick <r.poissonnier@codeur.online>
2021-03-31 14:05:56 +0200

    subtitle position

--- index.php
+++ index.php
@@ -2,10 +2,45 @@
 
 <main class="guideline">
     <div class="container pt-5 pb-5 guideline">
-        <div class="d-flex justify-content-center vertical-align-center guideline">
-            <h1 id="first-section__title" class="pt-5 mt-5">Hi, we are Debonaire. 
-            <br>Ready to create an art<br> form on your body. </h1>
-            <img id="first-section__img" class="guideline" src="images/blond-young-wife.png" alt="">
+    <!-- tempo -->
+        <div class="row guideline">
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+            <div class="col-md-1 guideline">col-1</div>
+        </div>
+    <!-- /tempo -->
+        <div class="row guideline">
+            <div class="col-md-2 col-sm-0 guideline">
+                <!-- nothing -->
+            </div>
+            <div style="z-index:1;" class="col-md-4 col-sm-12 guideline">
+                <h1 id="first-section__title" class="pt-5 mt-5">Hi, we are Debonaire. Ready to create an art form on your body. </h1>
+            </div>
+            <div class="col-md-6 col-sm-12 guideline">
+                <img id="first-section__img" class="guideline" src="images/blond-young-wife.png" alt="">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                        <!-- nothing -->
+                    </div>
+                    <div class="col-md-4 col-sm-12">
+                        <p id="first-section__subtitle" style="z-index:1; margin-top:-32vh;">
+                            Debonaire is natural product-based, hair design & boutique.
+                        </p>
+                    </div>
+                    <div class="col-md-2 col-sm-0">
+                        <!-- nothing -->
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
---
+++ main.js
@@ -0,0 +1,10 @@
+
+function resize() {
+    if(window.innerWidth <= 768 ){
+        document.getElementById('first-section__subtitle').style.marginTop = "0px";
+    } else {
+        document.getElementById('first-section__subtitle').style.marginTop = "-32vh";
+    }
+  }
+
+
