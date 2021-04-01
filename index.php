2b2cfea2366debc07b056dc88ad9d09b0dc8eeca
JasonMawerick <r.poissonnier@codeur.online>
2021-04-01 12:01:28 +0200

    third section ok

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php include 'header.php' ?>
 
-<main class="guideline">
     <div class="container pt-5 pb-5 guideline">
-    <!-- tempo -->
+        <!-- tempo -->
         <div class="row guideline">
             <div class="col-md-1 guideline">col-1</div>
             <div class="col-md-1 guideline">col-1</div>
@@ -17,7 +16,7 @@
             <div class="col-md-1 guideline">col-1</div>
             <div class="col-md-1 guideline">col-1</div>
         </div>
-    <!-- /tempo -->
+        <!-- /tempo -->
         <div class="row guideline">
             <div class="col-md-2 col-sm-0 guideline">
                 <!-- nothing -->
@@ -43,5 +42,51 @@
             </div>
         </div>
     </div>
+
+<div id="second-section__background" class="container-fluid guideline">
+    <div class="container guideline">
+            <div class="row guideline">
+                <div class="col-md-4 col-sm-12 guideline">
+                    <img src="images/ebony-young-wife.png" alt="">
+                </div>
+                <div class="col-md-4 col-sm-12 p-5 guideline">
+                    <h1>We’re focused.</h1>
+                    <p>We provide an exceptional service by only focusing on hair designing. Our goal is to provide a different type of experience by using responsible products.</p>
+                </div>
+                <div class="col-md-4 col-sm-12 guideline">
+                    <img src="images/aloevera.png" alt="">
+                </div>
+            </div>
+    </div>
+
+</div>
+<div id="third-section__background" class="container-fluid guideline pt-5 pb-5">
+    <div class ="row guideline">
+        <div class= "col-md-4 col-sm-12 ps-0 guideline">
+            <img class="mt-5 mb-5" src="images/long-hair-young-wife.png"alt="">
+</div>
+<div class ="col-md-4 col-sm-12 p-5 guideline">
+
+<div class="row">
+
+<div class="col-md-6">
+
+    <h1 id="third-section__title"> We love what we do . We are up to the task.</h1>
+    <p class="mt-5"> Cuts tailor made for your hair, making it easy for you to recreate your look every day. Debonaire team, makes you feel happy at ease and work with you to make you your most lovely.</p>
+
+</div>
+<div class="col-md-6"></div>
+
+</div>
+
+
+</div>
+<div class ="col-md-4 col-sm-12 guideline">
+<img src="images/sèche-cheveux.png" alt=""  id="third-section__image-right">
+</div>
+<div class ="col-md-4 col-sm-12 guideline">
+
+
+
 
 <?php include 'footer.php' ?>
